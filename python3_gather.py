dc9c6aad8dba8bf9166434ef99768f26a261651c
Doug Massay <douglaslmassay@gmail.com>
2025-04-01 15:07:22 -0400

    Test Python paths 13

--- python3_gather.py
+++ python3_gather.py
@@ -261,10 +261,11 @@ if __name__ == '__main__':
     #print(files)
 
     lib_dir = os.path.join(base_dest, 'usr', 'lib')
+    os.makedirs(lib_dir, exist_ok=True)
     bin_dir = os.path.join(base_dest, 'usr', 'bin')
     os.makedirs(bin_dir, exist_ok=True)
     site_dest = os.path.join(lib_dir, f'python{py_ver}', 'site-packages')
-    os.makedirs(site_dest, exist_ok=True)
+    #os.makedirs(site_dest, exist_ok=True)
     #dll_walk()
     #copy_pylib()
     copy_python()
